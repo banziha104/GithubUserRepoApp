5ad3a53188193e96a331d0aac6af5e5cb960a6b1
iyeongjun <they6687@naver.com>
2021-10-28 17:50:16 +0900

    Feat : Intent 필터 적용

--- app/src/main/java/com/lyj/githubuserrepoapp/presentation/activity/MainActivity.kt
+++ app/src/main/java/com/lyj/githubuserrepoapp/presentation/activity/MainActivity.kt
@@ -19,7 +19,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
-        val userName = intent.getStringExtra("userName") ?: "jakewharton"
+        val userName = intent.action ?: "jakewharton"
         viewModel
             .useCase
             .execute(userName)
