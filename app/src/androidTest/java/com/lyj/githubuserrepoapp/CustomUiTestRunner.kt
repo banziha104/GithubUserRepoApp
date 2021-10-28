2ebc81c57ad3a3963ff20ac58f8349faef725de6
iyeongjun <they6687@naver.com>
2021-10-28 16:10:31 +0900

    Chore : 필요한 라이브러리 명시

---
+++ app/src/androidTest/java/com/lyj/githubuserrepoapp/CustomUiTestRunner.kt
@@ -0,0 +1,17 @@
+package com.lyj.githubuserrepoapp
+
+import android.app.Application
+import android.content.Context
+import androidx.test.runner.AndroidJUnitRunner
+import dagger.hilt.android.testing.HiltTestApplication
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+class CustomUiTestRunner : AndroidJUnitRunner() {
+    override fun newApplication(cl: ClassLoader?, name: String?, context: Context?): Application {
+        return super.newApplication(cl, HiltTestApplication::class.java.name, context)
+    }
+}
