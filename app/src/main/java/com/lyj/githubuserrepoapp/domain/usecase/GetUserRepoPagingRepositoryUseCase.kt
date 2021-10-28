443306980c612cba649d5360485e4e00c64909c0
iyeongjun <they6687@naver.com>
2021-10-28 16:45:43 +0900

    Delete : Paging 제거

--- app/src/main/java/com/lyj/githubuserrepoapp/domain/usecase/GetUserRepoPagingRepositoryUseCase.kt
+++
@@ -1,9 +0,0 @@
-package com.lyj.githubuserrepoapp.domain.usecase
-
-import com.lyj.githubuserrepoapp.domain.repository.GithubApiRepository
-
-class GetUserRepoPagingRepositoryUseCase(
-    private val repository: GithubApiRepository
-) {
-
-}
