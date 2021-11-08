611b857e73a4dff8fef584eae42298c68ed9fa31
iyeongjun <they6687@naver.com>
2021-11-08 17:24:56 +0900

    Feat :  딥링크 구현

--- app/src/main/java/com/lyj/githubuserrepoapp/presentation/activity/MainActivity.kt
+++ app/src/main/java/com/lyj/githubuserrepoapp/presentation/activity/MainActivity.kt
@@ -2,6 +2,7 @@ package com.lyj.githubuserrepoapp.presentation.activity
 
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -12,27 +13,39 @@ import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
-
+//adb -s emulator-5554 shell am start -W  -a android.intent.action.VIEW -d testapp://repos/jakewharton com.lyj.githubuserrepoapp
     private val binding: ActivityMainBinding by lazy { ActivityMainBinding.inflate(layoutInflater) }
     private val viewModel: MainViewModel by viewModels<MainViewModel>()
+
+    companion object{
+        const val DEFAULT_URI = "testapp://repos/jakewharton"
+        const val REPOS_HOST = "repos"
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
-        val userName = intent.action ?: "jakewharton"
-        viewModel
-            .useCase
-            .execute(userName)
-            .observeOn(AndroidSchedulers.mainThread())
-            .subscribe({
-                binding
-                    .mainReclerview
-                    .apply {
-                        adapter = GithubAdapter(it)
-                        layoutManager = LinearLayoutManager(this@MainActivity)
-                    }
-            }, {
-                it.printStackTrace()
-            })
+        val data = intent.data
+        val path = data?.path
+        val host = data?.host
+
+        if (path != null && REPOS_HOST == host) {
+            viewModel
+                .useCase
+                .execute(path.drop(1))
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe({
+                    binding
+                        .mainReclerview
+                        .apply {
+                            adapter = GithubAdapter(it)
+                            layoutManager = LinearLayoutManager(this@MainActivity)
+                        }
+                }, {
+                    it.printStackTrace()
+                })
+        }else{
+            Toast.makeText(this,"잘못된 링크입니다",Toast.LENGTH_LONG).show()
+        }
     }
 }
